05722bf135133627e8bc0f96b28853ba7f2cd777
Aman Soomro <amansoomro062@gmail.com>
2025-02-16 22:33:28 +0000

    Initial Commit

---
+++ testplugin/testplugin.h
@@ -0,0 +1,18 @@
+//
+//  testplugin.h
+//  testplugin
+//
+//  Created by Aman Soomro on 16/02/2025.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for testplugin.
+FOUNDATION_EXPORT double testpluginVersionNumber;
+
+//! Project version string for testplugin.
+FOUNDATION_EXPORT const unsigned char testpluginVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <testplugin/PublicHeader.h>
+
+
